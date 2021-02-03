9bdc32b0a8b81f089257b33181d9bff21c80ea16
jwoods1 <jdwoodsy1@gmail.com>
2021-02-02 23:12:15 -0700

    added dev setup with docker

--- elixir/priv/repo/seeds.exs
+++ elixir/priv/repo/seeds.exs
@@ -9,3 +9,42 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Homework.{Users, Merchants, Transactions}
+
+users = [
+  %{
+    first_name: "Jason",
+    last_name: "Road",
+    dob: "Jun 20, 1985"
+  },
+  %{
+    first_name: "Kelly",
+    last_name: "Woods",
+    dob: "Jan 20, 1955"
+  },
+  %{
+    first_name: "Tanner",
+    last_name: "Dancer",
+    dob: "Nov 1, 1990"
+  },
+]
+
+Enum.each(users, fn(data) ->
+    Users.create_user(data)
+end)
+
+merchants = [
+  %{
+    description: "Developer help site",
+    name: "Stackoverflow",
+  },
+  %{
+    description: "Elixir Docs - package repo",
+    name: "Hex",
+  },
+]
+
+Enum.each(merchants, fn(data) ->
+  Merchants.create_merchant(data)
+end)
